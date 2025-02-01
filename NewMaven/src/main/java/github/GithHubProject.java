47751fb71398130d66df5c8aef41bb0672e12ba9
kumsu <kumsu@King_Vishwa>
2025-02-01 11:06:31 +0530

    Modified

--- NewMaven/src/main/java/github/GithHubProject.java
+++ NewMaven/src/main/java/github/GithHubProject.java
@@ -4,11 +4,10 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class GithHubProject {
-	public class Sample3 {
 		@Test
 		public void launch() {
 			Reporter.log("Sample Class Launched in GitHub", true);
-			
+			Reporter.log("Modified", true);
 		}
-	}
+	
 }
